022a83bc51ed33e6c61f2771846a67ab95c4c7a4
혜민\forbe <hyeomin9326@gmail.com>
2023-01-16 20:23:11 +0900

    프로젝트 생성

---
+++ src/test/java/com/projectpharmacy/ProjectPharmacyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectpharmacy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPharmacyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
