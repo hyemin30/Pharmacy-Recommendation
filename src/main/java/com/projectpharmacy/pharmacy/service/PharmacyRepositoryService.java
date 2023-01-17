41b71740e04c9984d983905144244145ee3c8db7
혜민\forbe <hyeomin9326@gmail.com>
2023-01-17 11:16:28 +0900

    약국 주소 업데이트 테스트
    트랜잭션있는 테스트와 없는 테스트 비교

---
+++ src/main/java/com/projectpharmacy/pharmacy/service/PharmacyRepositoryService.java
@@ -0,0 +1,43 @@
+package com.projectpharmacy.pharmacy.service;
+
+import com.projectpharmacy.pharmacy.entity.Pharmacy;
+import com.projectpharmacy.pharmacy.repository.PharmacyRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Objects;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class PharmacyRepositoryService {
+
+    private final PharmacyRepository pharmacyRepository;
+
+    //약국 주소 업데이트
+    @Transactional
+    public void updateAddress(Long id, String address) {
+        Pharmacy entity = pharmacyRepository.findById(id).orElse(null);
+
+        if (Objects.isNull(entity)) {
+            log.error("[PharmacyRepositoryService updateAddress] not found id: {}", id);
+            return;
+        }
+
+        entity.changePharmacyAddress(address);
+    }
+
+    // for test
+    public void updateAddressWithoutTransaction(Long id, String address) {
+        Pharmacy entity = pharmacyRepository.findById(id).orElse(null);
+
+        if(Objects.isNull(entity)) {
+            log.error("[PharmacyRepositoryService updateAddress] not found id: {}", id);
+            return;
+        }
+
+        entity.changePharmacyAddress(address);
+    }
+}
